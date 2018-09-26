575d7e639d3fc05b5c52b73ce04d7b08d6a93e90
roxyhuang <sixin_huang@outlook.com>
2018-09-26 17:13:22 +0800

    init project

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+// import { App } from 'app';
+
+console.log( '12323123');
+
+// render react DOM
+ReactDOM.render(
+        <div>123</div>,
+    document.getElementById('root')
+);
