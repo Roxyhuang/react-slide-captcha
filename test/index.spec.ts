e34d127744adf27293b0cd1b1ba9dbc4efea745d
roxyhuang <sixin_huang@outlook.com>
2018-09-28 14:56:31 +0800

    update unit test config

---
+++ test/index.spec.ts
@@ -0,0 +1,10 @@
+var expect = require('chai').expect;
+
+describe('<index />', () => {
+
+  it('should be defined', () => {
+    //const action = { type: 'ANY_TYPE' };
+    //expect(shallow(<App action={ action } />).contains(<div />)).toEqual(true);
+  })
+
+});
